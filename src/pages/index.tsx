c07621e41292a1b7faa459a3483146725843fc00
mos <fernandomosp@gmail.com>
2022-06-03 18:17:20 -0300

    :sparkles: Create style global

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,17 +1,19 @@
-import Header from 'next/head';
-import styles from '../style/style.module.scss';
+import Header from "next/head";
+import styles from "../style/style.module.scss";
 
 const Home = () => {
   return (
-    <div>
+    <>
       <Header>
         <title>Board - Organizando suas tarefas</title>
       </Header>
-      <h1 className={styles.title}>
-        Primeiro projeto em <span>NextJS</span>
-      </h1>
-    </div>
-  )
-}
+      <div>
+        <h1 className={styles.title}>
+          Primeiro projeto em <span>NextJS</span>
+        </h1>
+      </div>
+    </>
+  );
+};
 
-export default Home
+export default Home;
