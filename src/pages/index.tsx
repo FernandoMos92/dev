5826f1bf753887fe47ee644bfc0d65609a02c7e9
mos <fernandomosp@gmail.com>
2022-06-04 00:20:40 -0300

    :sparkles: Create image for developer

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,6 @@
 import Header from "next/head";
 import styles from "../style/style.module.scss";
+import Link from "next/link";
 
 const Home = () => {
   return (
@@ -7,11 +8,30 @@ const Home = () => {
       <Header>
         <title>Board - Organizando suas tarefas</title>
       </Header>
-      <div>
-        <h1 className={styles.title}>
-          Primeiro projeto em <span>NextJS</span>
-        </h1>
-      </div>
+      <main className={styles.contentContainer}>
+        <img src="/images/board-user.svg" alt="Ferramenta board" />
+
+        <section className={styles.callToAction}>
+          <h1>
+            Um ferramenta para seu dia a dia Escreva, planeje e organiza-se..
+          </h1>
+          <p>
+            <span>100% Gratuita</span> e online
+          </p>
+        </section>
+
+        <div className={styles.developer}>
+          <h4>Developer</h4>
+          <Link href="https://www.linkedin.com/in/fernando-mos/" passHref>
+            <a target="_blank">
+              <img
+                src="https://avatars.githubusercontent.com/u/60073911?s=400&u=53193e0e615b5e14adca7977346674341897b4fd&v=4"
+                alt="Developer"
+              />
+            </a>
+          </Link>
+        </div>
+      </main>
     </>
   );
 };
